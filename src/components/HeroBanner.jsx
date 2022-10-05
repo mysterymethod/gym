31965cb0ee01a5c5daacc9752e19a748fce5d22c
pranoy <chandra.pranay@yahoo.in>
2022-10-05 19:04:13 +0530

    home page left side is done and added img to the right

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Stack, Typography, Button } from '@mui/material'
+
+import HeroBannerImage from '../assets/images/banner.png'
+
 
 function HeroBanner() {
   return (
@@ -20,13 +23,47 @@ function HeroBanner() {
         Fitness Club
       </Typography>
 
-      <Typography>
+      <Typography
+        fontWeight={700}
+        sx={{fontSize: {lg : '44px', xs: '40px'}}}
+      >
         Sweat, Smile <br /> and Repeat
       </Typography>
 
-      <Typography>
+      <Typography 
+        fontSize='22px'
+        lineHeight='35px'
+        mb={2}
+      >
         Check out the most effective exercises
       </Typography>
+
+      <Button
+        variant='contained'
+        color='error'
+        href='#exercises'
+      >
+        Explore Exercises
+      </Button>
+
+      <Typography
+        fontWeight={600}
+        color='#ff2625'
+        sx = {{
+          opacity: 0.1,
+          display: {lg: 'block', xs: 'none'}
+        }}
+
+      >
+        Exercise
+      </Typography>
+
+
+      <img 
+        src={HeroBannerImage} 
+        alt='banner' 
+        className='hero-banner-img'
+      />
     </Box>
   )
 }
