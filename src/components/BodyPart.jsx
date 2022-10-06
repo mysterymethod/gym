6568a1560712aba91bc5deb96edf1c6013bfb022
pranoy <chandra.pranay@yahoo.in>
2022-10-06 19:15:44 +0530

    added the horixontal scroll

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -4,9 +4,15 @@ import { Stack, Typography } from '@mui/material'
 
 import Icon from '../assets/icons/gym.png'
 
-function BodyPart() {
+function BodyPart({item, setBodyPart, bodyPart}) {
   return (
-    <Stack>
+    <Stack
+      type='button'
+      alignItems='center'
+      justifyContent='center'
+      className='bodyPart-card'
+      sx={bodyPart === item ? { borderTop: '4px solid #FF2625', background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } : { background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' }}
+    >
       <img 
         src={Icon} 
         alt='dumbbell' 
